9fd82bb703405be1e625c527faf828a60eefadfe
DenisDubovtsev <77612500+DenisDubovtsev@users.noreply.github.com>
2021-02-11 10:22:24 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace testProjectForGit
+{
+    class Program
+    {
+        static void Main(string[] args) => Console.WriteLine("First branch Master!");
+        static void Main(string[] args) => Console.WriteLine("fbChange!");
+    }
+}
